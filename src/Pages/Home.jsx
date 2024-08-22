6df2c547c97c87516ad485a501cd41d8d9e34f5d
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-08-22 06:47:22 +0000

    Some fixes!

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import gsap from "gsap";
 
 const Home = () => {
   const [isJoinOpen, setJoinOpen] = useState(false);
+  const [isRegOpen, setRegOpen] = useState(false);
 
   function handleJoinOpenClick() {
     setJoinOpen(!isJoinOpen);
@@ -41,24 +42,58 @@ const Home = () => {
     }
   }
 
+  function handleRegisterOpenClick() {
+    setRegOpen(!isRegOpen);
+
+    if (isRegOpen) {
+      gsap.fromTo(
+        ".joinCard",
+        {
+          top: "200%",
+          duration: 0.5,
+          ease: "power3.out",
+        },
+        {
+          top: "50%",
+          duration: 0.5,
+          ease: "power3.out",
+        }
+      );
+    } else {
+      gsap.fromTo(
+        ".joinCard",
+        {
+          top: "50%",
+          duration: 0.5,
+          ease: "power3.out",
+        },
+        {
+          top: "200%",
+          duration: 0.5,
+          ease: "power3.out",
+        }
+      );
+    }
+  }
+
   return (
     <div className="home-page-main">
       <div className="joinCard">
-        <i
-          style={{
-            cursor: "pointer",
-            position: "absolute",
-            top: "20px",
-            left: "40px",
-          }}
-          onClick={handleJoinOpenClick}
-          class="fa-solid fa-xmark fa-2x"
-        ></i>
         <h1>Enter your school's UDISE code</h1>
         <input type="text" placeholder="Enter 11 digit UDISE code" />
         <button id="nextJoin">Next</button>
       </div>
-      <div className="registerCard"></div>
+      <div className="regCard">
+        <h1>Enter your school's detailes</h1>
+        <div className="regInputs">
+          <input type="text" placeholder="School Name" />
+          <input type="text" placeholder="Address" />
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+          <input type="text" placeholder="Pincode" />
+        </div>
+        <button id="nextJoin">Next</button>
+      </div>
       <div className="homepage">
         <div className="topRegister">
           <img src={logo} alt="" id="logo" />
@@ -69,7 +104,9 @@ const Home = () => {
           <div className="leftRegister">
             <h1>Register your E-SCHOOL Now!</h1>
             <div className="regButtons">
-              <button id="registerSchool">Register</button>
+              <button onClick={handleRegisterOpenClick} id="registerSchool">
+                Register
+              </button>
               <button onClick={handleJoinOpenClick} id="joinByID">
                 Join By UDISE
               </button>
@@ -88,7 +125,9 @@ const Home = () => {
           <h1>Welcome!</h1>
           <h4>Join or register a school to continue.</h4>
           <div className="regButtons">
-            <button id="registerSchool">Register</button>
+            <button onClick={handleRegisterOpenClick} id="registerSchool">
+              Register
+            </button>
             <button onClick={handleJoinOpenClick} id="joinByID">
               Join By UDISE
             </button>
